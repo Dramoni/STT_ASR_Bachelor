bf225e28fd42c4bcbfa0a6460912587eaf902303
Jacob <dramondramoni@gmail.com>
2022-11-24 22:14:43 +0100

    polished WakeWordDetector class

--- core/WakeWordDetector.py
+++ core/WakeWordDetector.py
@@ -9,8 +9,10 @@ from pydub import AudioSegment
 from pydub.playback import play
 from tensorflow.keras.models import load_model
 
+
 class WakeWordDetector:
-    def __init__(self, model_path=r"E:\Bachelor\saved_models\wake_word_conv2d_aug", response_path=r"E:\Bachelor\GLaDOS_Okay.wav"):
+    def __init__(self, model_path=r"E:\Bachelor\saved_models\wake_word_conv2d_aug",
+                 response_path=r"E:\Bachelor\GLaDOS_Okay.wav"):
         self.sample_rate = 22050
         self.seconds = 2
         self.n_mfcc = 20
@@ -22,21 +24,27 @@ class WakeWordDetector:
         print(self.model.summary())
         print("WakeWordDetector initialized!")
 
-    def listener(self):
+    def usage(self):
+        print(f"{'-' * 40}\nWakeWordDetector Usage:\n\tlisten() -> listen and predict\n{'-' * 40}")
+
+    def listen(self):
+        self.__voice_thread()
+
+    def __listener(self):
         print("listening now...")
         while True:
             rec = sd.rec(int(self.seconds * self.sample_rate), samplerate=self.sample_rate, channels=1)
             sd.wait()
             mfcc = librosa.feature.mfcc(y=rec.ravel(), sr=self.sample_rate, n_mfcc=self.n_mfcc)
-            mfcc_processed = mfcc # np.mean(mfcc.T, axis=0)
-            self.prediction_thread(mfcc_processed)
+            mfcc_processed = mfcc  # np.mean(mfcc.T, axis=0)
+            self.__prediction_thread(mfcc_processed)
             time.sleep(0.001)
 
-    def voice_thread(self):
-        listen_thread = threading.Thread(target=self.listener, name="ListeningFunction")
+    def __voice_thread(self):
+        listen_thread = threading.Thread(target=self.__listener, name="ListeningFunction")
         listen_thread.start()
 
-    def prediction(self, y):
+    def __prediction(self, y):
         pred = self.model.predict(np.expand_dims(y, axis=0))
         if pred[:, 1] > 0.95:
             print("WAKE WORD DETECTED!")
@@ -49,10 +57,6 @@ class WakeWordDetector:
             print(f"Confidence: {pred[:, 0]}")
         time.sleep(0.1)
 
-    def prediction_thread(self, y):
-        pred_thread = threading.Thread(target=self.prediction, name="PredictFunction", args=(y,))
+    def __prediction_thread(self, y):
+        pred_thread = threading.Thread(target=self.__prediction, name="PredictFunction", args=(y,))
         pred_thread.start()
-
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-wwd = WakeWordDetector()
-wwd.voice_thread()
