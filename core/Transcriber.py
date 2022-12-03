ff77a4fc7dec8c38c1a6d11f516d6f7fa8e694b5
Jacob <dramondramoni@gmail.com>
2022-12-04 00:32:09 +0100

    Update Transcriber.py

--- core/Transcriber.py
+++ core/Transcriber.py
@@ -7,10 +7,15 @@ from LiveAudioPreprocessor import LiveAudioPreprocessor
 
 class Transcriber:
     def __init__(self, model_path):
+        self.characters = [x for x in "abcdefghijklmnopqrstuvwxyzäöüß'?! "]
+        self.char_to_num = K.layers.StringLookup(vocabulary=self.characters, oov_token="")
+        self.num_to_char = K.layers.StringLookup(vocabulary=self.char_to_num.get_vocabulary(), oov_token="", invert=True)
+
         self.model = self.__load_model(model_path)
+        self.lap = LiveAudioPreprocessor()
 
     def transcribe(self, wav_path):
-        spec, audio, logits, transcriptions = self.__predict_wav(wav_path)
+        spec, audio, logits, transcriptions = self.__predict_wav(wav_path, greedy=True)
         return transcriptions
 
     def __load_model(self, model_path):
@@ -19,28 +24,18 @@ class Transcriber:
         print(model.summary())
         return model
 
-    def __predict_wav(self, wav_path):
-        lap = LiveAudioPreprocessor()
-        spec_wav, audio = lap.get_spectrogram_by_wav(wav_path)
+    def __predict_wav(self, wav_path, greedy=True):
+        spec_wav, audio = self.lap.get_spectrogram_by_wav(wav_path)
         spec = tf.expand_dims(spec_wav, 0)
         pred = self.model.predict(spec)
         pred_logits = np.squeeze(pred, axis=0)
         # print(f"Pred Shape: {pred_logits.shape} ({type(pred_logits)})")
 
-        res = self.__decode_batch_predictions(pred, greedy=True)
-        res02 = self.__decode_batch_predictions(pred, greedy=False)
-        # print(f"{res}\n{res02}")
+        res = self.__decode_batch_predictions(pred, greedy=greedy)
 
-        return spec_wav, audio, pred_logits, [res, res02]
+        return spec_wav, audio, pred_logits, res
 
     def __decode_batch_predictions(self, pred, greedy=True):
-        characters = [x for x in "abcdefghijklmnopqrstuvwxyzäöüß'?! "]
-        char_to_num = K.layers.StringLookup(vocabulary=characters, oov_token="")
-        num_to_char = K.layers.StringLookup(vocabulary=char_to_num.get_vocabulary(), oov_token="", invert=True)
-
-        # print(pred.shape)
-        # print('The vocabulary is: {} (size = {})'.format(char_to_num.get_vocabulary(), char_to_num.vocabulary_size()))
-
         input_len = np.ones(pred.shape[0]) * pred.shape[1]
         results = K.backend.ctc_decode(pred, input_length=input_len, greedy=greedy, beam_width=10, top_paths=5)
 
@@ -50,7 +45,7 @@ class Transcriber:
             # print(f"res: {res}")
             output_text = []
             for result in res:
-                result = tf.strings.reduce_join(num_to_char(result)).numpy().decode("utf-8")
+                result = tf.strings.reduce_join(self.num_to_char(result)).numpy().decode("utf-8")
                 output_text.append(result)
             all_outs.append(output_text)
             # print(f"tops: {all_outs}")
@@ -58,7 +53,7 @@ class Transcriber:
             top = results[0][0]
             output_text = []
             for result in top:
-                result = tf.strings.reduce_join(num_to_char(result)).numpy().decode("utf-8")
+                result = tf.strings.reduce_join(self.num_to_char(result)).numpy().decode("utf-8")
                 output_text.append(result)
         return output_text
 
@@ -80,6 +75,6 @@ class Transcriber:
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 transcriber = Transcriber("../saved_models/MASR_Model")
-transcriptions = transcriber.transcribe(wav_path="../STT_Inference/test02.wav")
-print(transcriptions[0])
+transcriptions = transcriber.transcribe(wav_path="../test_samples/test02.wav")
+print(transcriptions)
 '''
