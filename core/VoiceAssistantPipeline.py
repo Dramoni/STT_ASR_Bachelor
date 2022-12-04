8dd6b3ab4bdf5f8627c6320a89dec887ddc3cda7
Jacob <dramondramoni@gmail.com>
2022-12-04 17:50:06 +0100

    started voice assistant pipeline

--- core/VoiceAssistantPipeline.py
+++ core/VoiceAssistantPipeline.py
@@ -3,4 +3,5 @@ from core.Transcriber import Transcriber
 
 class VoiceAssistantPipeline:
     def __init__(self):
-        pass
+        self.wwd = WakeWordDetector(response_path="../responses/GLaDOS_Okay.wav", model_path="../saved_models/wake_word_conv2d_aug")
+        self.trans = Transcriber(model_path="../saved_models/MASR_Model")
