dea0f8234942b8999ea18bca4cf3d2d3b12f0894
Jacob <dramondramoni@gmail.com>
2022-12-04 17:54:16 +0100

    Update VoiceAssistantPipeline.py

--- core/VoiceAssistantPipeline.py
+++ core/VoiceAssistantPipeline.py
@@ -1,7 +1,13 @@
 from core.WakeWordDetector import WakeWordDetector
 from core.Transcriber import Transcriber
+from core.CommandManager import CommandManager
+from commands.CmdDefault import CmdDefault
 
 class VoiceAssistantPipeline:
     def __init__(self):
         self.wwd = WakeWordDetector(response_path="../responses/GLaDOS_Okay.wav", model_path="../saved_models/wake_word_conv2d_aug")
         self.trans = Transcriber(model_path="../saved_models/MASR_Model")
+
+        self.cm = CommandManager()
+        def_cmd = CmdDefault("Default Command")
+        self.cm.add_cmd("default", def_cmd)
